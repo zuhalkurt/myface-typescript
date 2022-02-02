bedeee57d7177e43344eadebdee652dc7b8dd613
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-02 13:35:48 +0000

    Added show/hide nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,13 @@ function changeColor() {
        post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` 
     }
 }
+
+function showNav() {
+    const nav = document.getElementsByClassName("display-nav")[0];
+    if (nav.style.display === 'flex') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'flex'
+    }
+} 
+
