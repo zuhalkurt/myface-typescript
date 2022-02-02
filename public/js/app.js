6c064de194fafddea76dcd0b48f263fbcdacfade
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-02 11:45:13 +0000

    Added button to set the background colour on each of the posts to be a random colour

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
-});
+const posts = document.getElementsByClassName("post");
+
+function changeColor() {
+    for (const post of posts) {
+       post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` 
+    }
+}
