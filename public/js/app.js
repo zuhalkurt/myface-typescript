010086906c6ec90198cf5ac9d1d1c994900d88ec
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-02-02 19:55:46 +0000

    nav-bar added but can't see on mobile version.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    const button = document.getElementById("my-button");
+    button.addEventListener("click", function() {
+        const posts = document.getElementsByClassName("post");
+        for (const post of posts) {
+        post.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` 
+}
+
+    })
+    
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
