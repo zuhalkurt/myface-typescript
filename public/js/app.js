283fea0a7faf0d016a35f13fecf0f693093ddfb9
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-02 14:36:02 +0000

    Added hamburger icon for navigation

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,11 @@ function changeColor() {
 }
 
 function showNav() {
-    const nav = document.getElementsByClassName("display-nav")[0];
-    if (nav.style.display === 'flex') {
+    const nav = document.getElementById("myLinks");
+    if (nav.style.display === 'block') {
         nav.style.display = 'none';
     } else {
-        nav.style.display = 'flex'
+        nav.style.display = 'block'
     }
 } 
 
